c16f123a1de01f480f5fdf4cd4373657ad14b119
rendy44 <deQueen44>
2019-02-13 08:36:14 +0700

    Add page title into page template

--- ss_wt4/page.php
+++ ss_wt4/page.php
@@ -13,6 +13,7 @@ while ( have_posts() ) : the_post(); ?>
     <main>
         <div class="container">
             <div class="content">
+                <h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
             </div>
         </div>
