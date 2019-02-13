4ab5593d90bc15920e9dc4225b4dffa2fb089b98
rendy44 <deQueen44>
2019-02-13 07:20:57 +0700

    Add sample to display custom meta in single team member

--- ss_wt4/archive.php
+++ ss_wt4/archive.php
@@ -5,3 +5,10 @@
  * Date: 2/12/2019
  * Time: 8:49 AM
  */
+
+get_header();
+
+if ( have_posts() ) :
+	echo "Adaaa";
+endif;
+get_footer();
--- ss_wt4/single-team-member.php
+++ ss_wt4/single-team-member.php
@@ -22,6 +22,11 @@ while ( have_posts() ) : the_post(); ?>
                         <p>You can show individual team member's detail here, but i choose shortcode from the plugin
                             instead</p>
 						<?php echo do_shortcode( '[ss_teammember id=' . get_the_ID() . ' use_bootstrap=true]' ); ?>
+                        <p>In case we really need to display individual field, so here's the name: <?php the_title(); ?>
+                            , position: <?php echo get_post_meta( get_the_ID(), 'ssteammember_position', true ); ?>,
+                            email: <?php echo get_post_meta( get_the_ID(), 'ssteammember_email', true ); ?>,
+                            website: <?php echo get_post_meta( get_the_ID(), 'ssteammember_website', true ); ?>, and so
+                            on.</p>
 
 						<?php
 						if ( is_active_sidebar( 'right_1' ) ) { ?>
