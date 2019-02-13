0c542b1cd526ebb1f7a58fc141499c08fb50acac
rendy44 <deQueen44>
2019-02-13 09:02:56 +0700

    Add template for single team member

--- ss_wt4/single-team-member.php
+++ ss_wt4/single-team-member.php
@@ -6,6 +6,8 @@
  * Time: 2:17 PM
  */
 
+global $ssWT4template;
+
 get_header();
 
 while ( have_posts() ) : the_post(); ?>
@@ -17,17 +19,7 @@ while ( have_posts() ) : the_post(); ?>
                 <div class="row">
                     <div class="col-sm-8">
 						<?php } ?>
-
-                        <h2><?php the_title(); ?></h2>
-                        <p>You can show individual team member's detail here, but i choose shortcode from the plugin
-                            instead</p>
-						<?php echo do_shortcode( '[ss_teammember id=' . get_the_ID() . ' use_bootstrap=true]' ); ?>
-                        <p>In case we really need to display individual field, so here's the name: <?php the_title(); ?>
-                            , position: <?php echo get_post_meta( get_the_ID(), 'ssteammember_position', true ); ?>,
-                            email: <?php echo get_post_meta( get_the_ID(), 'ssteammember_email', true ); ?>,
-                            website: <?php echo get_post_meta( get_the_ID(), 'ssteammember_website', true ); ?>, and so
-                            on.</p>
-
+						<?php echo $ssWT4template->render( 'single-team-member' ); ?>
 						<?php if ( is_active_sidebar( 'right_1' ) ) { ?>
                     </div>
                     <div class="col-sm-4">
--- ss_wt4/templates/single-team-member.php
+++ ss_wt4/templates/single-team-member.php
@@ -1,7 +1,9 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ASUS
- * Date: 2/13/2019
- * Time: 7:22 AM
- */
+<h2><?php the_title(); ?></h2>
+<p>You can show individual team member's detail here, but i choose shortcode from the plugin
+	instead</p>
+<?php echo do_shortcode( '[ss_teammember id=' . get_the_ID() . ' use_bootstrap=true]' ); ?>
+<p>In case we really need to display individual field, so here's the name: <?php the_title(); ?>
+	, position: <?php echo get_post_meta( get_the_ID(), 'ssteammember_position', true ); ?>,
+	email: <?php echo get_post_meta( get_the_ID(), 'ssteammember_email', true ); ?>,
+	website: <?php echo get_post_meta( get_the_ID(), 'ssteammember_website', true ); ?>, and so
+	on.</p>
