f37c4280fca8ee6f45bc291961ee7f5e7ccabd13
rendy44 <deQueen44>
2019-02-13 08:29:20 +0700

    Remove unused files

--- ss_wt4/comments.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ASUS
- * Date: 2/12/2019
- * Time: 8:52 AM
- */
--- ss_wt4/sidebar.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ASUS
- * Date: 2/12/2019
- * Time: 8:50 AM
- */
